5dc877b215edfed8c7101e3e8a21b5f3658799b7
chengtoann <158062425@qq.com>
2018-06-01 23:45:02 +0800

    add js

--- js/1.js
+++ js/1.js
@@ -1,3 +1,40 @@
 var i=1;
 var list=document.getElementsByTagName("li");  
-function 
+        //轮播函数  
+       function xiaopin(){  
+            if(i>4){  
+               i=i%5;  
+           }  
+           for(var m=0;m<list.length;m++){  
+               if(list[m].style.backgroundColor=="green"){  
+                    list[m].style.backgroundColor="black";  
+                }  
+                list[i].style.backgroundColor="green";  
+            }  
+            document.getElementById("img").src="imgs/"+i+".png";  
+          i++;  
+       }  
+       //设置并开启一个定时器  
+       var a=setInterval(xiaopin, 2000);  
+        //鼠标摸进去事件  
+        function mousemove(obj){  
+            for(var m=0;m<list.length;m++){  
+                if(list[m].style.backgroundColor=="green"){  
+                    list[m].style.backgroundColor="black";  
+                    document.getElementById("img").src="imgs/"+m+".png";  
+                    i=m+1;  
+                }  
+            }  
+            obj.style.backgroundColor="green";  
+            clearInterval(a);  
+       }  
+        //鼠标摸出来时间  
+        function mouseout(obj){  
+            setTimeout(function(){  
+ 
+           }, 2000);  
+           a=setInterval(xiaopin, 2000);  
+       }  
+  
+ 
+
